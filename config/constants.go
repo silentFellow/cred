39089446e9f5859aaa3c5eb52720614c2eb462ff
silentFellow <gowtham.sri+github@zohomail.in>
2025-01-24 21:13:11 +0530

    Add gpg-key-id

--- config/constants.go
+++ config/constants.go
@@ -6,6 +6,7 @@ import (
 )
 
 type constants struct {
+  GpgKey string
   Home string
   Config string
   StorePath string
@@ -19,7 +20,13 @@ func initConstants() constants {
   home := getEnv("HOME", "/home")
   defaultConfig := fmt.Sprintf("%v/.config", home)
 
+  gpgKey := ""
+  if file, err := os.ReadFile(fmt.Sprintf("%v/.cred-store/.gpg-id", home)); err == nil {
+    gpgKey = string(file)
+  }
+
   return constants {
+    GpgKey: gpgKey,
     Home: home,
     Config: getEnv("XDG_CONFIG_HOME", defaultConfig),
     StorePath: fmt.Sprintf("%v/.cred-store", home),
