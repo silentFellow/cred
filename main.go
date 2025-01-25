f392d53b329e895a8d9b51a479915cbd4111e2c0
silentFellow <gowtham.sri+github@zohomail.in>
2025-01-25 12:13:52 +0530

    Add check gpg installation

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/silentFellow/cred-store/cmd"
+import (
+	"fmt"
+	"os/exec"
+
+	"github.com/silentFellow/cred-store/cmd"
+)
 
 func main() {
+	gpgCmd := exec.Command("gpg", "--version")
+	if _, err := gpgCmd.CombinedOutput(); err != nil {
+		fmt.Println("GPG is not installed. Please install GPG to use this tool")
+		return
+	}
+
 	cmd.Execute()
 }
