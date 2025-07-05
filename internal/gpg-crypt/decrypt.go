dfa998d9ad0c8ac6ecf1b5806549aa892e5ae048
silentFellow <gowtham.sri+github@zohomail.in>
2025-07-05 20:44:13 +0530

    Fix invalid passphrase handling

--- internal/gpg-crypt/decrypt.go
+++ internal/gpg-crypt/decrypt.go
@@ -3,6 +3,7 @@ package gpgcrypt
 import (
 	"bytes"
 	"errors"
+	"strings"
 
 	"github.com/silentFellow/cred/config"
 	"github.com/silentFellow/cred/internal/utils"
@@ -18,9 +19,16 @@ func Decrypt(filePath string) (string, error) {
 	cmd := utils.SetCmd(utils.CmdConfig{}, "gpg", "--decrypt", filePath)
 
 	var outBuffer bytes.Buffer
+	var errBuffer bytes.Buffer
 	cmd.Stdout = &outBuffer
+	cmd.Stderr = &errBuffer
 
 	if err := cmd.Run(); err != nil {
+		errMsg := strings.ToLower(errBuffer.String())
+		if strings.Contains(errMsg, "no passphrase") || strings.Contains(errMsg, "bad passphrase") {
+			return "", errors.New("wrong passphrase")
+		}
+
 		return "", err
 	}
 
